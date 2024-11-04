0cf339ae72c9e87ec75177ffb8fbe4717b5cf92a
Marek <marek.lewanczyk@outlook.com>
2024-11-04 20:13:33 +0100

    Spring Boot Initializr

---
+++ src/main/java/pl/pjatk/MATLOG/MatlogApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.MATLOG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatlogApplication.class, args);
+	}
+
+}
