b8fec05fe3f4a138b53135d29befec0deed59d9b
Marek <marek.lewanczyk@outlook.com>
2024-12-04 20:37:03 +0100

    Add Set<PrivateLesson> to StudentUser class

--- src/main/java/pl/pjatk/MATLOG/domain/StudentUser.java
+++ src/main/java/pl/pjatk/MATLOG/domain/StudentUser.java
@@ -1,12 +1,18 @@
 package pl.pjatk.MATLOG.domain;
 
+import lombok.Getter;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Set;
 
 /**
  * Concrete class representing Student in application
  */
+@Getter
 public final class StudentUser extends User {
+
+    private final Set<PrivateLesson> privateLessons;
 
     /**
      * Method that returns builder and starts chaining creation of the object
@@ -20,6 +26,13 @@ public final class StudentUser extends User {
      * Concrete implementation of the Builder in User abstract class
      */
     static class StudentUserBuilder extends Builder<StudentUserBuilder> {
+
+        private Set<PrivateLesson> privateLessons;
+
+        public StudentUserBuilder withPrivateLessons(Set<PrivateLesson> privateLessons) {
+            this.privateLessons = privateLessons;
+            return self();
+        }
 
         @Override
         protected StudentUserBuilder self() {
@@ -44,6 +57,7 @@ public final class StudentUser extends User {
      */
     private StudentUser(StudentUserBuilder builder) {
         super(builder);
+        this.privateLessons = builder.privateLessons;
         addAuthority(new SimpleGrantedAuthority("STUDENT_USER"));
     }
 
