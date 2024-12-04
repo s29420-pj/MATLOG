baa5f78552b570f1d1cee6a645a55005ff137513
Mateusz <mateusz.szotynski3@gmail.com>
2024-12-04 11:10:54 +0100

    test

--- src/main/java/pl/pjatk/MATLOG/userManagement/UserController.java
+++ src/main/java/pl/pjatk/MATLOG/userManagement/UserController.java
@@ -2,8 +2,10 @@ package pl.pjatk.MATLOG.userManagement;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pl.pjatk.MATLOG.domain.User;
 
 @RestController
--- src/test/java/pl/pjatk/MATLOG/userManagement/unitTests/UserControllerTests.java
+++ src/test/java/pl/pjatk/MATLOG/userManagement/unitTests/UserControllerTests.java
@@ -1,4 +1,35 @@
 package pl.pjatk.MATLOG.userManagement.unitTests;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+import pl.pjatk.MATLOG.domain.StudentUser;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class UserControllerTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void helloMessage() throws Exception {
+        mvc.perform(post("/user/controller/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(StudentUser.builder()
+                        .withFirstName("Matt")
+                        .withLastName("Thew")
+                        .withEmailAddress("example@example.com")
+                        .withPassword("testPassword!")
+                        .build()))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+    }
 }
