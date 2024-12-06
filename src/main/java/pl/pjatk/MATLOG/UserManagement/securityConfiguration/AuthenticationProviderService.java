b20a7dc31e72bc3ea7bc1baa33d85f48e6f78620
Mateusz <mateusz.szotynski3@gmail.com>
2024-12-06 14:20:26 +0100

    Added javadoc to the AuthenticationProviderService

--- src/main/java/pl/pjatk/MATLOG/UserManagement/securityConfiguration/AuthenticationProviderService.java
+++ src/main/java/pl/pjatk/MATLOG/UserManagement/securityConfiguration/AuthenticationProviderService.java
@@ -9,6 +9,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import pl.pjatk.MATLOG.UserManagement.user.SecurityUser;
 
+/**
+ * Component that is responsible for handling data evaluation. It checks
+ * if provided password is same as in the database then Authentication object is returned
+ */
 @Component
 public class AuthenticationProviderService implements AuthenticationProvider {
 
@@ -20,6 +24,15 @@ public class AuthenticationProviderService implements AuthenticationProvider {
         this.passwordEncoder = passwordEncoder;
     }
 
+    /**
+     * Method that checks if provided raw password is same as password stored in the database.
+     * If so returns UsernamePasswordAuthenticationToken.
+     * If passwords doesn't match then BadCredentialsException is thrown
+     * @param authentication the authentication request object.
+     * @return Authentication object
+     * @throws AuthenticationException when something goes wrong
+     * @throws BadCredentialsException when passwords doesn't match
+     */
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String username = authentication.getName();
@@ -33,6 +46,13 @@ public class AuthenticationProviderService implements AuthenticationProvider {
         }
     }
 
+    /**
+     * Method that is used by AuthenticationManager to check if
+     * AuthenticationProvider supports this kind of authentication method.
+     * @param authentication authentication method
+     * @return UsernamePasswordAuthenticationToken that tells, this AuthenticationProvider supports
+     * username and password type of authentication.
+     */
     @Override
     public boolean supports(Class<?> authentication) {
         return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
