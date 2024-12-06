2a541726e7ec445c38f418c73481f5be8fb3fdd8
Mateusz <mateusz.szotynski3@gmail.com>
2024-12-06 13:38:17 +0100

    Repaired MongoUserDetailsService to work with UserDAOMapper

--- src/main/java/pl/pjatk/MATLOG/UserManagement/securityConfiguration/MongoUserDetailsService.java
+++ src/main/java/pl/pjatk/MATLOG/UserManagement/securityConfiguration/MongoUserDetailsService.java
@@ -3,10 +3,15 @@ package pl.pjatk.MATLOG.UserManagement.securityConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+import pl.pjatk.MATLOG.Domain.Enums.Role;
+import pl.pjatk.MATLOG.Domain.User;
 import pl.pjatk.MATLOG.UserManagement.user.SecurityUser;
+import pl.pjatk.MATLOG.UserManagement.user.mappers.UserDAOMapper;
+import pl.pjatk.MATLOG.UserManagement.user.persistance.UserDAO;
 import pl.pjatk.MATLOG.UserManagement.user.persistance.UserRepository;
 import pl.pjatk.MATLOG.UserManagement.user.UserService;
 
+import java.util.Optional;
 import java.util.function.Supplier;
 
 @Component
@@ -14,16 +19,25 @@ public class MongoUserDetailsService implements UserDetailsService {
 
     private final UserService userService;
     private final UserRepository userRepository;
+    private final UserDAOMapper userDAOMapper;
 
-    public MongoUserDetailsService(UserService userService, UserRepository repository) {
+    public MongoUserDetailsService(UserService userService, UserRepository userRepository, UserDAOMapper userDAOMapper) {
         this.userService = userService;
-        this.userRepository = repository;
+        this.userRepository = userRepository;
+        this.userDAOMapper = userDAOMapper;
     }
 
     @Override
     public SecurityUser loadUserByUsername(String username) {
-        Supplier<UsernameNotFoundException> ex = () ->
-                new UsernameNotFoundException("User with that email address does not exist.");
-        return new SecurityUser(userRepository.findByEmailAddress(username).orElseThrow(ex));
+        Optional<UserDAO> userDAO = userRepository.findByEmailAddress(username);
+        if (userDAO.isEmpty()) {
+            throw new UsernameNotFoundException("User with that email address does not exist.");
+        }
+        return new SecurityUser(returnUserFromUserDAO(userDAO.get()));
+    }
+
+    private User returnUserFromUserDAO(UserDAO userDAO) {
+        return userDAO.role() == Role.STUDENT ?
+                userDAOMapper.mapToStudentUser(userDAO) : userDAOMapper.mapToTutorUser(userDAO);
     }
 }
