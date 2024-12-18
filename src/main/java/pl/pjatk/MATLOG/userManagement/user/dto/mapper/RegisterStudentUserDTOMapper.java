11758d5cd4bfa638c870f41d3e2ca01b2c26316c
Mateusz <mateusz.szotynski3@gmail.com>
2024-12-18 09:44:54 +0100

    Added logic to RegisterStudetUserDTOMapper and RegisterTutorUserDTOMapper

--- src/main/java/pl/pjatk/MATLOG/userManagement/user/dto/mapper/RegisterStudentUserDTOMapper.java
+++ src/main/java/pl/pjatk/MATLOG/userManagement/user/dto/mapper/RegisterStudentUserDTOMapper.java
@@ -1,7 +1,19 @@
 package pl.pjatk.MATLOG.UserManagement.user.dto.mapper;
 
 import org.springframework.stereotype.Component;
+import pl.pjatk.MATLOG.Domain.StudentUser;
+import pl.pjatk.MATLOG.UserManagement.user.dto.UserDTO;
 
 @Component
 public class RegisterStudentUserDTOMapper {
+
+    public StudentUser createStudentUser(UserDTO userDTO) {
+        return StudentUser.builder()
+                .withFirstName(userDTO.firstName())
+                .withLastName(userDTO.lastName())
+                .withEmailAddress(userDTO.emailAddress())
+                .withPassword(userDTO.password())
+                .withDateOfBirth(userDTO.dateOfBirth())
+                .build();
+    }
 }
--- src/main/java/pl/pjatk/MATLOG/userManagement/user/dto/mapper/RegisterTutorUserDTOMapper.java
+++ src/main/java/pl/pjatk/MATLOG/userManagement/user/dto/mapper/RegisterTutorUserDTOMapper.java
@@ -1,8 +1,19 @@
 package pl.pjatk.MATLOG.UserManagement.user.dto.mapper;
 
 import org.springframework.stereotype.Component;
+import pl.pjatk.MATLOG.Domain.TutorUser;
+import pl.pjatk.MATLOG.UserManagement.user.dto.UserDTO;
 
 @Component
 public class RegisterTutorUserDTOMapper {
 
+    public TutorUser createTutorUser(UserDTO userDTO) {
+        return TutorUser.builder()
+                .withFirstName(userDTO.firstName())
+                .withLastName(userDTO.lastName())
+                .withEmailAddress(userDTO.emailAddress())
+                .withPassword(userDTO.password())
+                .withDateOfBirth(userDTO.dateOfBirth())
+                .build();
+    }
 }
