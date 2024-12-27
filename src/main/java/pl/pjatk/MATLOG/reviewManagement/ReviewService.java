3cb2a87a5fe91a0c8f6cf63d460bcee5c6f6f9ac
Mateusz <mateusz.szotynski3@gmail.com>
2024-12-27 16:39:40 +0100

    Added method to obtain Tutors ReviewDTO by email address

--- src/main/java/pl/pjatk/MATLOG/reviewManagement/ReviewService.java
+++ src/main/java/pl/pjatk/MATLOG/reviewManagement/ReviewService.java
@@ -2,6 +2,9 @@ package pl.pjatk.MATLOG.reviewManagement;
 
 import org.springframework.stereotype.Service;
 import pl.pjatk.MATLOG.Domain.Review;
+import pl.pjatk.MATLOG.reviewManagement.dto.ReviewDTO;
+import pl.pjatk.MATLOG.reviewManagement.mapper.ReviewDAOMapper;
+import pl.pjatk.MATLOG.reviewManagement.mapper.ReviewDTOMapper;
 
 import java.util.List;
 
@@ -9,10 +12,19 @@ import java.util.List;
 public class ReviewService {
 
     private final ReviewRepository reviewRepository;
+    private final ReviewDAOMapper daoMapper;
+    private final ReviewDTOMapper dtoMapper;
 
-    public ReviewService(ReviewRepository reviewRepository) {
+    public ReviewService(ReviewRepository reviewRepository, ReviewDAOMapper daoMapper, ReviewDTOMapper dtoMapper) {
         this.reviewRepository = reviewRepository;
+        this.daoMapper = daoMapper;
+        this.dtoMapper = dtoMapper;
     }
 
-
+    public List<ReviewDTO> getTutorReviewsDTOByEmailAddress(String emailAddress) {
+        return reviewRepository.findAllByStudent_EmailAddress(emailAddress).stream()
+                .map(daoMapper::create)
+                .map(dtoMapper::create)
+                .toList();
+    }
 }
