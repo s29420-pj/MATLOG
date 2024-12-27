9a06e28389cf5211f9d5db7fb5528e9c79b54803
Mateusz <mateusz.szotynski3@gmail.com>
2024-12-27 16:35:17 +0100

    Made ReviewDTOMapper as a bean

--- src/main/java/pl/pjatk/MATLOG/reviewManagement/mapper/ReviewDTOMapper.java
+++ src/main/java/pl/pjatk/MATLOG/reviewManagement/mapper/ReviewDTOMapper.java
@@ -1,9 +1,11 @@
 package pl.pjatk.MATLOG.reviewManagement.mapper;
 
+import org.springframework.stereotype.Component;
 import pl.pjatk.MATLOG.Domain.Review;
 import pl.pjatk.MATLOG.UserManagement.user.student.mapper.StudentUserReviewDTOMapper;
 import pl.pjatk.MATLOG.reviewManagement.dto.ReviewDTO;
 
+@Component
 public class ReviewDTOMapper {
 
     private final StudentUserReviewDTOMapper studentUserReviewDTOMapper;
