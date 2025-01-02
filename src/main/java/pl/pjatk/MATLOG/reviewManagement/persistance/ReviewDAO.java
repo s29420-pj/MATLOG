34dc21475915d212945fb169ec3f5f07d4fae5f5
Mateusz <mateusz.szotynski3@gmail.com>
2025-01-02 18:18:30 +0100

    ReviewDAO javadoc completed

--- src/main/java/pl/pjatk/MATLOG/reviewManagement/persistance/ReviewDAO.java
+++ src/main/java/pl/pjatk/MATLOG/reviewManagement/persistance/ReviewDAO.java
@@ -1,13 +1,23 @@
 package pl.pjatk.MATLOG.reviewManagement.persistance;
 
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.core.mapping.*;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
 import pl.pjatk.MATLOG.Domain.Enums.Stars;
 import pl.pjatk.MATLOG.UserManagement.user.student.persistance.StudentUserDAO;
 import pl.pjatk.MATLOG.UserManagement.user.tutor.persistance.TutorUserDAO;
 
 import java.time.LocalDateTime;
 
+/**
+ * Review representation in database.
+ * @param id Identification of the Review.
+ * @param comment Comment which is created by Student.
+ * @param rate Rate of Tutor.
+ * @param dateAndTimeOfComment Creation time and date of Review.
+ * @param student Student who created Review.
+ * @param tutor Tutor which is concerned.
+ */
 @Document("review")
 public record ReviewDAO(@MongoId
                         String id,
