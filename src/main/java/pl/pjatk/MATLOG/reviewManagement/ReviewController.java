a1a418a474fb1148beaecc85d3a4b0a7b2dd4687
Mateusz <mateusz.szotynski3@gmail.com>
2025-01-02 18:32:27 +0100

    Completed javadoc for ReviewController

--- src/main/java/pl/pjatk/MATLOG/reviewManagement/ReviewController.java
+++ src/main/java/pl/pjatk/MATLOG/reviewManagement/ReviewController.java
@@ -9,6 +9,9 @@ import pl.pjatk.MATLOG.reviewManagement.dto.ReviewLookUpDTO;
 
 import java.util.List;
 
+/**
+ * Review controller layer which is used by front-end to communicate with ReviewManagement application.
+ */
 @RestController
 @RequestMapping("/review/controller")
 public class ReviewController {
@@ -19,18 +22,34 @@ public class ReviewController {
         this.reviewService = reviewService;
     }
 
+    /**
+     * Endpoint which returns List of ReviewLookUpDTO from provided Tutors email address.
+     * @param emailAddress Email address of the Tutor user.
+     * @return List of ReviewLookUpDTO
+     */
     @GetMapping("/find/byTutor/{emailAddress}")
     public ResponseEntity<List<ReviewLookUpDTO>> findTutorReviewsDTO(@PathVariable String emailAddress) {
         List<ReviewLookUpDTO> tutorReviewsDTOByEmailAddress = reviewService.getTutorReviewsDTOByEmailAddress(emailAddress);
         return ResponseEntity.status(HttpStatus.OK).body(tutorReviewsDTOByEmailAddress);
     }
 
+    /**
+     * Endpoint which returns List of ReviewLookUpDTO from provided Students email address.
+     * @param emailAddress Email address of the Student user.
+     * @return List of ReviewLookUpDTO.
+     */
     @GetMapping("/find/byStudent/{emailAddress}")
     public ResponseEntity<List<ReviewLookUpDTO>> findStudentReviewsDTO(@PathVariable String emailAddress) {
         List<ReviewLookUpDTO> studentReviewDTOByEmailAddress = reviewService.getStudentReviewsDTOByEmailAddress(emailAddress);
         return ResponseEntity.status(HttpStatus.OK).body(studentReviewDTOByEmailAddress);
     }
 
+    /**
+     * Endpoint which is used to save provided Review to the database.
+     * @param reviewCreationDTO DTO representation of the Review.
+     * @return ResponseEntity with CREATED Http Status.
+     * @throws AuthenticationException When either student user or tutor user do not exist.
+     */
     @PostMapping("/create")
     public ResponseEntity<Void> createReview(@RequestBody ReviewCreationDTO reviewCreationDTO) throws AuthenticationException {
         reviewService.saveReview(reviewCreationDTO);
