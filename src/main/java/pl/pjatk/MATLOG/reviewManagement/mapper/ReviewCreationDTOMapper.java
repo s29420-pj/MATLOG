427931eab230a08a082147c552707bbf1a0ccdb7
Mateusz <mateusz.szotynski3@gmail.com>
2025-01-02 18:08:46 +0100

    Completed javadoc for ReviewDAOMapper

--- src/main/java/pl/pjatk/MATLOG/reviewManagement/mapper/ReviewCreationDTOMapper.java
+++ src/main/java/pl/pjatk/MATLOG/reviewManagement/mapper/ReviewCreationDTOMapper.java
@@ -6,6 +6,10 @@ import pl.pjatk.MATLOG.Domain.StudentUser;
 import pl.pjatk.MATLOG.Domain.TutorUser;
 import pl.pjatk.MATLOG.reviewManagement.dto.ReviewCreationDTO;
 
+/**
+ * Mapper class which is responsible for <br></>
+ * converting ReviewCreationDTO into Review
+ */
 @Component
 public class ReviewCreationDTOMapper {
 
