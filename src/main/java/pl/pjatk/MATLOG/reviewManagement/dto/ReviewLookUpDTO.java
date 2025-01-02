86fe6ef72a264d290032e2ea15134575089e5307
Mateusz <mateusz.szotynski3@gmail.com>
2025-01-02 18:07:17 +0100

    Completed javadoc for ReviewLookUpDTO

--- src/main/java/pl/pjatk/MATLOG/reviewManagement/dto/ReviewLookUpDTO.java
+++ src/main/java/pl/pjatk/MATLOG/reviewManagement/dto/ReviewLookUpDTO.java
@@ -5,6 +5,13 @@ import pl.pjatk.MATLOG.UserManagement.user.student.dto.StudentUserReviewLookUpDT
 
 import java.time.LocalDateTime;
 
+/**
+ * Record that represents Review when is displayed on Tutor's profile.
+ * @param comment Comment of user.
+ * @param rate Overall rate (1 - 5).
+ * @param dateAndTimeOfReview Creation date and time of the review.
+ * @param studentUser Students representation in Tutors profile.
+ */
 public record ReviewLookUpDTO(String comment,
                               Stars rate,
                               LocalDateTime dateAndTimeOfReview,
