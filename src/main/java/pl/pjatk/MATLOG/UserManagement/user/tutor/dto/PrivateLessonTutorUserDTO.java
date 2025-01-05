48a20007743e79c53dd350430d0ffd105e28f846
Marek <marek.lewanczyk@outlook.com>
2025-01-05 17:47:29 +0100

    Excluded Google Calendar Module, removed HTMLReport and JavaDoc

---
+++ src/main/java/pl/pjatk/MATLOG/UserManagement/user/tutor/dto/PrivateLessonTutorUserDTO.java
@@ -0,0 +1,7 @@
+package pl.pjatk.MATLOG.UserManagement.user.tutor.dto;
+
+public record PrivateLessonTutorUserDTO(
+        String id,
+        String firstName,
+        String lastName,
+        String email) {}
--- src/main/java/pl/pjatk/MATLOG/googleCalendar/mapper/PrivateLesssonCalendarMapper.java
+++ src/main/java/pl/pjatk/MATLOG/googleCalendar/mapper/PrivateLesssonCalendarMapper.java
@@ -1,6 +1,6 @@
 package pl.pjatk.MATLOG.googleCalendar.mapper;
 
-import pl.pjatk.MATLOG.Domain.PrivateLesson;
+import pl.pjatk.MATLOG.Domain.PrivateLesson.PrivateLesson;
 import pl.pjatk.MATLOG.googleCalendar.dto.PrivateLessonCalendarDTO;
 
 public class PrivateLesssonCalendarMapper {
