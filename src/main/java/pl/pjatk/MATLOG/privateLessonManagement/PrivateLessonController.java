117a0847b65883656a7feea75b61cb769a8406c1
Marek <marek.lewanczyk@outlook.com>
2025-01-21 23:33:50 +0100

    Added CrossOrigin to localhosted frontend

--- src/main/java/pl/pjatk/MATLOG/privateLessonManagement/PrivateLessonController.java
+++ src/main/java/pl/pjatk/MATLOG/privateLessonManagement/PrivateLessonController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/private_lesson/")
+@CrossOrigin(origins = "http://localhost:4200")
 public class PrivateLessonController {
 
     private final PrivateLessonService privateLessonService;
