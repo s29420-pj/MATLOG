69ce9fcc12980a063cb570f047edcbf16965a026
Mateusz-Szotynski <157532482+Mateusz-Szotynski@users.noreply.github.com>
2025-01-22 19:23:49 +0100

    Completed controller layer in studentUser

--- src/main/java/pl/pjatk/MATLOG/userManagement/studentUser/StudentUserController.java
+++ src/main/java/pl/pjatk/MATLOG/userManagement/studentUser/StudentUserController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.pjatk.MATLOG.domain.enums.Role;
 import pl.pjatk.MATLOG.userManagement.securityConfiguration.AuthenticationProviderService;
+import pl.pjatk.MATLOG.userManagement.studentUser.dto.StudentUserChangeEmailAddressDTO;
 import pl.pjatk.MATLOG.userManagement.studentUser.dto.StudentUserChangePasswordDTO;
 import pl.pjatk.MATLOG.userManagement.studentUser.dto.StudentUserProfileDTO;
 import pl.pjatk.MATLOG.userManagement.user.dto.CredentialsDTO;
@@ -50,6 +51,12 @@ public class StudentUserController {
     @PutMapping("/change/password")
     public ResponseEntity<Void> changePassword(@RequestBody StudentUserChangePasswordDTO studentUserChangePasswordDTO) {
         studentUserService.changePassword(studentUserChangePasswordDTO);
+        return ResponseEntity.accepted().build();
+    }
+
+    @PutMapping("/change/emailAddress")
+    public ResponseEntity<Void> changeEmailAddress(@RequestBody StudentUserChangeEmailAddressDTO studentUserChangeEmailAddressDTO) {
+        studentUserService.changeEmailAddress(studentUserChangeEmailAddressDTO);
         return ResponseEntity.accepted().build();
     }
 }
